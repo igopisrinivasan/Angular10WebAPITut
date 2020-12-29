0ba8a8ce6ae0e5b22868388082c2a40e515a2376
Gopi Srinivasan <igopisrinivasan@gmail.com>
2020-12-29 11:33:46 +0530

    navigation js added

--- ui/my-app/src/Navigation.js
+++ ui/my-app/src/Navigation.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
 export class Navigation extends Component {
   render() {
     return (
-      <div className="mt-5 d-flex justify-content-left">
-        This is Navigation Page
-      </div>
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <NavLink className="d-inline p-2 bg-dark text-white" to="/">
+              Home
+            </NavLink>
+            <NavLink
+              className="d-inline p-2 bg-dark text-white"
+              to="/department"
+            >
+              Department
+            </NavLink>
+            <NavLink className="d-inline p-2 bg-dark text-white" to="/employee">
+              Employee
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
