c9195b08c9118b35c645046f53fd4da8498ad7bf
Gopi Srinivasan <igopisrinivasan@gmail.com>
2020-12-29 11:59:18 +0530

    react Integration done to the project

--- ui/my-app/src/App.js
+++ ui/my-app/src/App.js
@@ -1,25 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-import { Home } from "./Home";
-import { Department } from "./Department";
-import { Employee } from "./Employee";
-import { Navigation } from "./Navigation";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {Home} from './Home';
+import {Department} from './Department';
+import {Employee} from './Employee';
+import {Navigation} from './Navigation';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="container">
-        <h3 className="m-3 d-flex justify-content-center">React JS Tutorial</h3>
+    <div className="container">
+     <h3 className="m-3 d-flex justify-content-center">
+       React JS Tutorial
+     </h3>
 
-        <Navigation />
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/department" component={Department} />
-          <Route path="/employee" component={Employee} />
-        </Switch>
-      </div>
+     <Navigation/>
+
+     <Switch>
+       <Route path='/' component={Home} exact/>
+       <Route path='/department' component={Department}/>
+       <Route path='/employee' component={Employee}/>
+     </Switch>
+    </div>
     </BrowserRouter>
   );
 }
